4ab9fa1e1a15d4c79179eaa7100e12505021a12a
solo <solohuckstep@gmail.com>
2023-02-07 22:48:44 -0500

    fixed certain erros in th resolvers

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -6,7 +6,7 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        return await User.findOne({ _id: context.user._Id });
+        return await User.findOne({ _id: context.user._id });
       }
       throw new AuthenticationError('You need to be logged in!');
     },
